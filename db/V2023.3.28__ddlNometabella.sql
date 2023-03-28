6c7a47b90438f714a9f9d0be3064d0e42fdd795b
Bandellostefano <126395138+Bandellostefano@users.noreply.github.com>
2023-03-28 11:36:48 +0200

    inserite altre tabelle

--- db/V2023.3.28__ddlNometabella.sql
+++ db/V2023.3.28__ddlNometabella.sql
@@ -10,3 +10,21 @@ CREATE TABLE affetti_svil.domanda (
 	CONSTRAINT domanda_pkey PRIMARY KEY (id),
 	CONSTRAINT domanda_fk_contraente_fkey FOREIGN KEY (fk_contraente) REFERENCES affetti_svil.contraente(id)
 );
+
+CREATE TABLE affetti_svil.contratto (
+	data_contratto date NOT NULL,
+	scadenza_contratto date NOT NULL,
+	stato varchar NOT NULL,
+	utenti varchar NOT NULL,
+	data_insert date NOT NULL,
+	data_update date NOT NULL
+);
+
+CREATE TABLE affetti_svil.assegnatario (
+	nome varchar NOT NULL,
+	cognome varchar NOT NULL,
+	dati_anagrafici varchar NOT NULL,
+	utenti varchar NOT NULL,
+	data_insert date NOT NULL,
+	data_update date NOT NULL
+);
