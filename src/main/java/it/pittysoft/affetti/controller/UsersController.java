43928ee0f1c66d7ec737a2c51c56934b7a572562
giosueanastasi <anastasi@consulenzeinformatiche.net>
2023-03-29 01:53:02 +0200

    change library

---
+++ src/main/java/it/pittysoft/affetti/controller/UsersController.java
@@ -0,0 +1,39 @@
+package it.pittysoft.affetti.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.pittysoft.affetti.entity.Users;
+import it.pittysoft.affetti.links.UserLinks;
+import it.pittysoft.affetti.service.UsersService;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/")
+public class UsersController {
+	
+	@Autowired
+	UsersService usersService;
+	
+	@GetMapping(path = UserLinks.LIST_USERS)
+    public ResponseEntity<?> listUsers() {
+        log.info("UsersController:  list users");
+        List<Users> resource = usersService.getUsers();
+        return ResponseEntity.ok(resource);
+    }
+	
+	@PostMapping(path = UserLinks.ADD_USER)
+	public ResponseEntity<?> saveUser(@RequestBody Users user) {
+        log.info("UsersController:  list users");
+        Users resource = usersService.saveUser(user);
+        return ResponseEntity.ok(resource);
+    }
+}
