7bf8189de19b1a16e71308f6e65ac51b79a915ad
Bandellostefano <126395138+Bandellostefano@users.noreply.github.com>
2023-03-31 09:18:19 +0200

    modifica data.sql be

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -25,3 +25,20 @@ INSERT INTO posto (ID, LOCULO,fornice) VALUES
   (1, 'first', 'last 1'),
   (2, 'first', 'last 2'),
   (3, 'first', 'last 3');
+  
+DROP TABLE IF EXISTS domanda;
+
+CREATE TABLE domanda (
+	id serial4 NOT NULL,
+	protocollo int4 NOT NULL,
+	data_domanda date NOT NULL,
+	stato varchar,
+	utenti varchar NOT NULL,
+	data_insert date NOT NULL,
+	data_update date NOT NULL,
+	fk_contraente serial4 NOT NULL,
+
+INSERT INTO domanda (id, protocollo, data_domanda,stato,utenti, data_insert, data_update, fk_contraente) VALUES
+  (1, 55430, 2022-02-01, 2022-01-08, 'lavorazione','tommaso', 2022-01-08, 2022-01-08, 010),
+  (2, 54789, 2022-02-05, 2022-01-07, 'lavorazione', 'tommaso', 2022-01-07, 2022-01-07, 020),
+  (3, 42587, 2022-02-07, 2022-01-01, 'accolta', 'tommaso', 2022-01-01, 2022-01-01, 008);
