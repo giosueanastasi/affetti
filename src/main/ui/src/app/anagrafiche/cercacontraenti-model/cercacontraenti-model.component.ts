3c54c34679d28413d65d34d63ec8db7ee5594315
Bandellostefano <126395138+Bandellostefano@users.noreply.github.com>
2023-04-22 00:01:38 +0200

    parte mancate del fe manca il collegamento del bottone

--- src/main/ui/src/app/anagrafiche/cercacontraenti-model/cercacontraenti-model.component.ts
+++ src/main/ui/src/app/anagrafiche/cercacontraenti-model/cercacontraenti-model.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { AppService } from 'src/app/app.service';
+import { Contraente1 } from 'src/app/app-state/models';
+declare var $ : any;
 
 @Component({
   selector: 'app-cercacontraenti-model',
@@ -7,4 +12,48 @@ import { Component } from '@angular/core';
 })
 export class CercacontraentiModelComponent {
 
-}
+  cercacontraenteCount = 0;
+
+  destroy$: Subject<boolean> = new Subject<boolean>();
+  
+  contraente1: Contraente1 = new Contraente1();
+   errorMessage: string = "";
+  
+    @Output() save =  new EventEmitter<any>();
+    constructor(private appService: AppService) { }
+  
+    contraenti: any[] = [];
+  
+  
+    filtraCercacontraenti(contraenteForm: Contraente1) {
+      this.appService.cercaCercacontraenti(contraenteForm).pipe(takeUntil(this.destroy$)).subscribe((data: any) => {
+        this.cercacontraenteCount =data.length;
+        this.contraenti = data.contraenti;
+        });
+    }
+  
+    getAllCercaontraenti() {
+      this.appService.getContraenti().pipe(takeUntil(this.destroy$)).subscribe((contraenti: any[]) => {
+      this.cercacontraenteCount =contraenti.length;
+          this.contraenti = contraenti;
+      });
+    }
+  
+  
+    ngOnInit() {
+      console.log('esegui all contraente1 on init');
+      this.getAllCercaontraenti();
+      }
+  
+      showCercacontraentiModal(){
+        $('#cercacontraentiModal').modal('show');
+      }
+  
+      assegnaContraente(contraenti: any[]){
+  
+        this.save.emit(contraenti);
+        $('#cercacontraentiModal').modal('hide');
+        
+      
+      }
+  }
---
+++ src/main/ui/src/app/app-state/models/contraente1.model.ts
@@ -0,0 +1,14 @@
+export class Contraente1 {
+	nome: String;
+	cognome: String;
+	codice_fiscale: String;
+	protocollo: String;
+	comune_residenza: String;
+	provincia_residenza: String;
+	via_residenza: String;
+	nomeAss:String;
+	cognomeAss:String;
+	contratto:String;
+	checked: boolean;
+
+}
