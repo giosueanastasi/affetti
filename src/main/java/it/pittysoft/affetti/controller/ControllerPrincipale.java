e5f468254d28c4b5b2739abeeed98716d80bdaef
Marco-Carangelo <marcocarangelo2016@gmail.com>
2025-01-30 16:15:33 +0100

    AF-12 Modificato il metodo listComuni nel controller principale ed il metodo getComuni in comuniService per passare una lista di oggetti comuniModel

--- src/main/java/it/pittysoft/affetti/controller/ControllerPrincipale.java
+++ src/main/java/it/pittysoft/affetti/controller/ControllerPrincipale.java
@@ -24,6 +24,7 @@ import it.pittysoft.affetti.links.PostoLinks;
 import it.pittysoft.affetti.links.UserLinks;
 import it.pittysoft.affetti.model.ContrattoSearchRequest;
 import it.pittysoft.affetti.model.ContrattoSearchResponse;
+import it.pittysoft.affetti.model.ComuniResponse;
 import it.pittysoft.affetti.model.ContraentiRequest;
 import it.pittysoft.affetti.model.ContraentiResponse;
 import it.pittysoft.affetti.model.ContrattoModel;
@@ -207,7 +208,7 @@ public class ControllerPrincipale {
 	@GetMapping(path = ComuneLinks.LIST_COMUNI)
     public ResponseEntity<?> listComuni() {
         log.info("ApiController:  list Comuni");
-        List<Comuni> resource = comuniService.getComuni();
+        ComuniResponse resource = comuniService.getComuni();
         return ResponseEntity.ok(resource);
     }
 	
--- src/main/java/it/pittysoft/affetti/service/ComuniService.java
+++ src/main/java/it/pittysoft/affetti/service/ComuniService.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 import it.pittysoft.affetti.entity.Comuni;
+import it.pittysoft.affetti.model.ComuniModel;
+import it.pittysoft.affetti.model.ComuniRequest;
+import it.pittysoft.affetti.model.ComuniResponse;
 import it.pittysoft.affetti.repository.ComuniRepository;
 
 
@@ -18,8 +21,28 @@ public class ComuniService {
         this.comuniRepository = comuniRepository;
     }
 
-    public List<Comuni> getComuni() {
-        return comuniRepository.findAll();
+    public ComuniResponse getComuni() {
+    	
+    	List<Comuni> comuni = comuniRepository.findAll();
+    	ComuniResponse response = new ComuniResponse();
+    	
+    	for (Comuni comune : comuni) {
+    		
+    		ComuniModel cm = new ComuniModel();
+    		cm.setId(comune.getId());
+    		cm.setNome(comune.getNome());
+    		cm.setCodComuneIstat(comune.getCodComuneIstat());
+    		cm.setCodCatastComune(comune.getCodCatastComune());
+    		cm.setProvincia(comune.getProvincia());
+    		cm.setCodProv(comune.getCodProv());
+    		cm.setDescProv(comune.getDescProv());
+    		cm.setCodReg(comune.getCodReg());
+    		cm.setDescReg(comune.getDescReg());
+    		
+    		response.getComuni().add(cm);
+    	}
+    	
+    	return response;
     }
     
     public Comuni saveComune(Comuni comuni) {
