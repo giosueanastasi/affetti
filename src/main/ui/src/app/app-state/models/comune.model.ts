1962431751ddba8470a486a8b45838be02845c0a
Marco-Carangelo <marcocarangelo2016@gmail.com>
2025-02-22 19:13:39 +0100

    Eliminati campi non utilizzati nel model della classe comune lato FE

--- src/main/ui/src/app/app-state/models/comune.model.ts
+++ src/main/ui/src/app/app-state/models/comune.model.ts
@@ -1,13 +1,7 @@
 export class Comune {
   id: number;
   nome: string;
-  codComuneIstat: string;
-  codCatastComune: string;
   provincia: string;
-  codProv: string;
-  descProv: string;
-  codReg: string;
-  descReg: string;
   stato: string;
 }
 
