7eb89b0c0a2485b0caf143fbabbb897f921b8363
Marco-Carangelo <marcocarangelo2016@gmail.com>
2025-03-18 17:31:52 +0100

    fix: slash mancanti nei nuovi percorsi della domanda

--- src/main/java/it/pittysoft/affetti/links/DomandaLinks.java
+++ src/main/java/it/pittysoft/affetti/links/DomandaLinks.java
@@ -9,6 +9,6 @@ public class DomandaLinks {
     public static final String ADD_DOMANDA = "/domanda";
     public static final String ADD_DOMANDA_FULL = "/domandaFull";
     public static final String SEARCH_DOMANDE = "/search_domande";
-    public static final String GENERA_PROTOCOLLO = "genera_protocollo_domanda";
-    public static final String STAMPA_DOMANDA = "stampa_domanda";
+    public static final String GENERA_PROTOCOLLO = "/genera_protocollo_domanda";
+    public static final String STAMPA_DOMANDA = "/stampa_domanda";
 }
