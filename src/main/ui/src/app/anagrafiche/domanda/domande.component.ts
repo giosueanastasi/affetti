2f1ac73dae8a4db0f575c5441e975be1c7c0fb45
MattiaCanopoli <canopoli.mattia@gmail.com>
2025-06-22 11:16:06 +0200

    feat: invocata cercaDomande() in ngOnInit per popolare la lista all'avvio

--- src/main/ui/src/app/anagrafiche/domanda/domande.component.ts
+++ src/main/ui/src/app/anagrafiche/domanda/domande.component.ts
@@ -1,57 +1,69 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { AppService } from '../../app.service';
-import { takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
-import { ContraentiModelComponent } from '../contraenti-model/contraenti-model.component';
-import { Contratto, Domanda, Posto, Assegnatario } from 'src/app/app-state/models';
-import { DomandaModelComponent } from '../domanda-model/domanda-model.component';
-import { ContrattoModelComponent } from '../contratto-model/contratto-model.component';
-
-
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+  ViewChild,
+} from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { AppService } from "../../app.service";
+import { takeUntil } from "rxjs/operators";
+import { Subject } from "rxjs";
+import { ContraentiModelComponent } from "../contraenti-model/contraenti-model.component";
+import {
+  Contratto,
+  Domanda,
+  Posto,
+  Assegnatario,
+} from "src/app/app-state/models";
+import { DomandaModelComponent } from "../domanda-model/domanda-model.component";
+import { ContrattoModelComponent } from "../contratto-model/contratto-model.component";
 
 @Component({
-  selector: 'app-domande',
-  templateUrl: './domande.component.html',
-  styleUrls: ['./domande.component.css']
+  selector: "app-domande",
+  templateUrl: "./domande.component.html",
+  styleUrls: ["./domande.component.css"],
 })
-
 export class DomandeComponent implements OnInit, OnDestroy {
-
-  constructor(private appService: AppService) { }
+  constructor(private appService: AppService) {}
 
   @ViewChild(DomandaModelComponent) child: DomandaModelComponent | undefined;
-  @ViewChild(ContrattoModelComponent) childContratto: ContrattoModelComponent | undefined;
-
+  @ViewChild(ContrattoModelComponent) childContratto:
+    | ContrattoModelComponent
+    | undefined;
 
   selectedDomanda: Domanda = new Domanda();
 
-  title = 'angular-nodejs-example';
+  title = "angular-nodejs-example";
 
   domandaForm = new FormGroup({
-    nome: new FormControl('', Validators.nullValidator),
-    cognome: new FormControl('', Validators.nullValidator),
-    tipologia: new FormControl('', Validators.nullValidator),
-    codice_fiscale: new FormControl('', Validators.nullValidator),
-    numero_protocollo: new FormControl('', Validators.nullValidator),
-    data_protocollo_iniziale: new FormControl('', Validators.nullValidator),
-    data_protocollo_finale: new FormControl('', Validators.nullValidator),
-    stato: new FormControl('', Validators.nullValidator)
+    nome: new FormControl("", Validators.nullValidator),
+    cognome: new FormControl("", Validators.nullValidator),
+    tipologia: new FormControl("", Validators.nullValidator),
+    codice_fiscale: new FormControl("", Validators.nullValidator),
+    numero_protocollo: new FormControl("", Validators.nullValidator),
+    data_protocollo_iniziale: new FormControl("", Validators.nullValidator),
+    data_protocollo_finale: new FormControl("", Validators.nullValidator),
+    stato: new FormControl("", Validators.nullValidator),
   });
 
   domande: any[] = [];
   domandaCount = 0;
 
-  selectedContratto: Contratto = new Contratto;
+  selectedContratto: Contratto = new Contratto();
 
   destroy$: Subject<boolean> = new Subject<boolean>();
 
-
   getAllDomande() {
-    this.appService.getDomande().pipe(takeUntil(this.destroy$)).subscribe((domande: any[]) => {
-      this.domandaCount = domande.length;
-      this.domande = domande;
-    });
+    this.appService
+      .getDomande()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((domande: any[]) => {
+        this.domandaCount = domande.length;
+        this.domande = domande;
+      });
   }
 
   ngOnDestroy() {
@@ -64,34 +76,36 @@ export class DomandeComponent implements OnInit, OnDestroy {
   }
 
   cercaDomande(cercaDomandaForm: FormGroup) {
-    this.appService.cercaDomandeService(cercaDomandaForm.value).pipe(takeUntil(this.destroy$)).subscribe((data: any) => {
-      this.domande = data.domande;
-    });
+    this.appService
+      .cercaDomandeService(cercaDomandaForm.value)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data: any) => {
+        this.domande = data.domande;
+      });
   }
 
-
-
   ngOnInit() {
-    console.log('esegui all domande on init');
+    console.log("esegui all domande on init");
+    this.cercaDomande(this.domandaForm);
     // this.getAllDomande();
   }
 
   editDomandaRequest(item: any) {
-    let domanda = new Domanda;
-	
-	const posto = new Posto();
-	posto.id = item.fk_posto;
-	posto.fornice = item.fornice;
-	posto.loculo = item.loculo;
-	
-	const assegnatario = new Assegnatario();
-	assegnatario.id = item.fk_assegnatario;
-	assegnatario.nome = item.nomeAss;
-	assegnatario.cognome = item.cognomeAss;
-	
-	domanda.id = item.id;
-	domanda.posto = posto;
-	domanda.assegnatario = assegnatario;
+    let domanda = new Domanda();
+
+    const posto = new Posto();
+    posto.id = item.fk_posto;
+    posto.fornice = item.fornice;
+    posto.loculo = item.loculo;
+
+    const assegnatario = new Assegnatario();
+    assegnatario.id = item.fk_assegnatario;
+    assegnatario.nome = item.nomeAss;
+    assegnatario.cognome = item.cognomeAss;
+
+    domanda.id = item.id;
+    domanda.posto = posto;
+    domanda.assegnatario = assegnatario;
     domanda.data_protocollo = item.dataProtocollo;
     domanda.protocollo = item.numeroProtocolloDomanda;
     domanda.tipologia = item.tipologia;
@@ -122,8 +136,7 @@ export class DomandeComponent implements OnInit, OnDestroy {
   }
 
   saveDomandaWatcher(domanda: Domanda) {
-
-    let domandaIndex = this.domande.findIndex(item => item.id === domanda.id);
+    let domandaIndex = this.domande.findIndex((item) => item.id === domanda.id);
     if (domandaIndex !== -1) {
       this.domande[domandaIndex] = domanda;
     } else {
@@ -165,6 +178,4 @@ export class DomandeComponent implements OnInit, OnDestroy {
     this.selectedContratto = Object.assign({}, contratto);
     this.childContratto?.showContrattoModal();
   }
-
-
 }
